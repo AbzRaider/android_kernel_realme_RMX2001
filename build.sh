81b050ee15b1ab28f6f7e226444a05dc60c7c441
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2023-04-14 12:06:48 +0000

    build_script : Fixup.and Azrael Rebrand

--- build.sh
+++ build.sh
@@ -7,11 +7,22 @@ source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
 ccache -M 100G
 export ARCH=arm64
-export KBUILD_BUILD_HOST=Anupam_Roy
-export KBUILD_BUILD_USER="Gorilla669"
+export KBUILD_BUILD_HOST="MARK // DEVS"
+export KBUILD_BUILD_USER="AbzRaider"
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
 
-[ -d "out" ] && rm -rf AnyKernel && rm -rf out || mkdir -p out
+if ! [ -d "out" ]; then
+	echo "Kernel OUT Directory Not Found . Making Again"
+mkdir out
+
+else
+
+	
+	sleep 5
+	echo "out directory already exists , Making Dirty Build !! "
+	echo "If you want to clean Build , just rm -rf out"
+	
+fi
 
 make O=out ARCH=arm64 RMX2001_defconfig
 
@@ -32,13 +43,14 @@ make -j$(nproc --all) O=out \
 
 function zupload()
 {
-git clone --depth=1 https://github.com/Johny8988/AnyKernel3.git AnyKernel
+if  [ -d "AnyKernel" ]; then	
+	rm -rf AnyKernel
+fi	
+git clone --depth=1 https://github.com/AbzRaider/AnyKernel_RMX2001 -b main AnyKernel
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
-zip -r9 ThunderStorm-lto-KERNEL-RMX2001.zip *
-curl -sL https://git.io/file-transfer | sh
-./transfer wet ThunderStorm-lto-KERNEL-RMX2001.zip
+zip -r9 Azrael-OSS-KERNEL-RMX2001-v6.zip *
+curl --upload-file "Azrael-OSS-KERNEL-RMX2001-v6.zip" https://free.keep.sh
 }
-
 compile
 zupload
